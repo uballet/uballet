ed9865c12781aad4a5098ddac582deee8daec022
Flavio Ondarts <flavio.ondarts@world50.com>
2024-05-23 16:17:10 -0300

    Added Pill component and stories

---
+++ src/components/Pill.tsx
@@ -0,0 +1,26 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+interface PillProps {
+    text: string
+}
+function Pill({ text }: PillProps) {
+    return (
+        <View style={PillStyles.container}>
+            <Text>{text}</Text>
+        </View>
+    );
+}
+
+const PillStyles = StyleSheet.create({
+    container: {
+        borderRadius: 50,
+        paddingHorizontal: 16,
+        paddingVertical: 8,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 1,
+        borderColor: 'purple'
+    }
+})
+
+export { Pill }
