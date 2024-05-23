42949dcc249f547868d28b5ce08844892e3f9760
Flavio Ondarts <flavio.ondarts@world50.com>
2024-05-23 16:10:51 -0300

    Added basic support for storybook

--- App.tsx
+++ App.tsx
@@ -2,6 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log({ storybook: process.env.EXPO_PUBLIC_STORYBOOK });
+  if (process.env.EXPO_PUBLIC_STORYBOOK === "true") {
+    console.log({
+      storybook: "true"
+    })
+    const Storybook = require("./.storybook").default;
+
+    return <Storybook />
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
