f867c8b2371b4e1829f921de5e701bf0720c2aec
Flavio Ondarts <flavio.ondarts@world50.com>
2024-05-26 12:57:02 -0300

    Fix typo

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,7 @@
 import { Slot } from 'expo-router';
 
 export default function App() {
-  if (process.env.EXPO_PUBLIC_STORYBOOK === "truer") {
+  if (process.env.EXPO_PUBLIC_STORYBOOK === "true") {
     const Storybook = require("./../../.storybook").default;
 
     return <Storybook />
