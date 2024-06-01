017c081c2ca74026c595189599efe76fd3c8bfd9
Geronimo Illescas <gillescas@fi.uba.ar>
2024-06-01 16:19:56 -0300

    Add basic setup for notifications

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Slot } from 'expo-router';
+import { View, Text } from 'react-native';
+import * as Notifications from "expo-notifications";
+import { registerForPushNotificationsAsync } from './notifications/register';
 
 export default function App() {
   if (process.env.EXPO_PUBLIC_STORYBOOK === "true") {
@@ -7,6 +10,53 @@ export default function App() {
 
     return <Storybook />
   }
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState<Notifications.Notification | undefined>(undefined);
+  const notificationListener = useRef<Notifications.Subscription>();
+  const responseListener = useRef<Notifications.Subscription>();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync()
+      .then((token) => setExpoPushToken(token ?? ''))
+      .catch((error: any) => setExpoPushToken(`${error}`));
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      notificationListener.current &&
+        Notifications.removeNotificationSubscription(
+          notificationListener.current,
+        );
+      responseListener.current &&
+        Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+  return (
+    <View
+      style={{ flex: 1, alignItems: 'center', justifyContent: 'space-around' }}
+    >
+      <Text>Your Expo push token: {expoPushToken}</Text>
+      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+        <Text>
+          Title: {notification && notification.request.content.title}{' '}
+        </Text>
+        <Text>Body: {notification && notification.request.content.body}</Text>
+        <Text>
+          Data:{' '}
+          {notification && JSON.stringify(notification.request.content.data)}
+        </Text>
+      </View>
+    </View>
+  );
   return (
     <Slot />
   );
