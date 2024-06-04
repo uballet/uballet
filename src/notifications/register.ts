971b1f1215b8eec8c7e807cdbc13dc53b49026cb
Geronimo Illescas <gillescas@fi.uba.ar>
2024-06-04 20:46:36 -0300

    set notification handler for foreground

--- src/notifications/register.ts
+++ src/notifications/register.ts
@@ -22,6 +22,14 @@ export async function registerForPushNotificationsAsync() {
     });
   }
 
+  Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: true,
+      shouldSetBadge: false,
+    }),
+  });
+
   const { status: existingStatus } = await Notifications.getPermissionsAsync();
   let finalStatus = existingStatus;
   if (existingStatus !== "granted") {
