c16217d295a7edd4b58d9bcab2d9e345ab8d9636
Flavio Ondarts <flavio.ondarts@world50.com>
2024-06-05 16:02:27 -0300

    Added/updated dependencies for alchemy poc

---
+++ index.js
@@ -0,0 +1,3 @@
+import "node-libs-react-native/globals.js";
+import "react-native-get-random-values";
+import "expo-router/entry";
--- metro.config.js
+++ metro.config.js
@@ -5,6 +5,13 @@ const path = require("path");
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+config.resolver.extraNodeModules = {
+  ...config.resolver.extraNodeModules,
+  ...require("node-libs-react-native"),
+  crypto: require.resolve("crypto-browserify"),
+  stream: require.resolve("stream-browserify"),
+};
+
 if (process.env.STORYBOOK === "true") {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const { generate } = require("@storybook/react-native/scripts/generate");
