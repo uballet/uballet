700ba4b5b16059ffc13e70d502e8682228ee53af
Geronimo Illescas <gillescas@fi.uba.ar>
2024-06-17 17:51:35 -0300

    add react native paper

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,18 +1,33 @@
-import { Slot } from 'expo-router';
-import React from 'react';
-import { useNotificationObserver } from '../notifications/observer';
-import { registerForPushNotificationsAsync } from '../notifications/register';
+import { Slot } from "expo-router";
+import React from "react";
+import { useNotificationObserver } from "../notifications/observer";
+import { registerForPushNotificationsAsync } from "../notifications/register";
+import {
+  MD3LightTheme as DefaultTheme,
+  PaperProvider,
+} from "react-native-paper";
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "tomato",
+    secondary: "yellow",
+  },
+};
 
 export default function App() {
   if (process.env.EXPO_PUBLIC_STORYBOOK === "true") {
     const Storybook = require("./../../.storybook").default;
 
-    return <Storybook />
+    return <Storybook />;
   }
 
-  registerForPushNotificationsAsync()
+  registerForPushNotificationsAsync();
   useNotificationObserver();
   return (
-    <Slot />
+    <PaperProvider theme={theme}>
+      <Slot />
+    </PaperProvider>
   );
 }
