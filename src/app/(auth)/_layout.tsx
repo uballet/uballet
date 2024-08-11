ca52c294097e4f964573352aac03886042eb333c
fpondarts <flperez@fi.uba.ar>
2024-08-11 03:38:51 -0300

    Refactor redirection in auth layout

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,5 +1,4 @@
-import { useEffect } from "react";
-import { Redirect, router, Slot, usePathname } from "expo-router";
+import { Redirect, Slot, usePathname } from "expo-router";
 import { ActivityIndicator } from "react-native";
 import { useLightAccount } from "../../hooks/useLightAccount";
 import { useAuthContext } from "../../providers/AuthProvider";
@@ -9,18 +8,17 @@ export default function AuthenticatedLayout() {
     const { user, passkeysOnboarded } = useAuthContext()
     const account = useLightAccount()
     const { isSuccess, isLoading, passkeys } = useUserPasskeys()
-
     const pathname = usePathname()
     const isRegisterPasskeys = pathname.includes('/register-passkey')
 
-    useEffect(() => {
-        if (isSuccess && !passkeys?.length && !isRegisterPasskeys && !passkeysOnboarded) {
-            router.navigate('/(auth)/register-passkey')
-        }
-    }, [isSuccess, router, isRegisterPasskeys, passkeys])
-
     if (!user?.verified) {
         return <Redirect href={'/(public)'} />
+    }
+
+    const shouldRedirect = isSuccess && !passkeys?.length && !isRegisterPasskeys && !passkeysOnboarded
+
+    if (shouldRedirect) {
+        return <Redirect href={'/(auth)/register-passkey'} />
     }
 
     if (!account || isLoading) {
