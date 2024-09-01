a32459425ab4d793768031dea63696b872bd9e50
Santiago Tissoni <60752879+stissoni@users.noreply.github.com>
2024-09-01 14:12:13 -0300

    Fix env file

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -1,9 +1,9 @@
 // uballet api
-export const UBALLET_API_URL = "http://192.168.0.8:3000"
+export const UBALLET_API_URL = process.env.EXPO_PUBLIC_UBALLET_API_URL;
 
 // alchemy api
-export const ALCHEMY_API_URL = process.env.EXPO_PUBLIC_ALCHEMY_API_URL
-export const ALCHEMY_POLICY_ID = process.env.EXPO_PUBLIC_ALCHEMY_POLICY_ID
+export const ALCHEMY_API_URL = process.env.EXPO_PUBLIC_ALCHEMY_API_URL;
+export const ALCHEMY_POLICY_ID = process.env.EXPO_PUBLIC_ALCHEMY_POLICY_ID;
 
 // storybook
-export const IS_STORYBOOK = process.env.EXPO_PUBLIC_STORYBOOK === 'true'
+export const IS_STORYBOOK = process.env.EXPO_PUBLIC_STORYBOOK === "true";
