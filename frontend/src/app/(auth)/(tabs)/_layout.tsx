0e5b700c6aec9bac0abae824a3f1907c4606590a
Santiago Tissoni <60752879+stissoni@users.noreply.github.com>
2024-09-17 15:25:40 -0300

    Home icon

--- frontend/src/app/(auth)/(tabs)/_layout.tsx
+++ frontend/src/app/(auth)/(tabs)/_layout.tsx
@@ -10,67 +10,68 @@ import {
 const { Navigator } = createMaterialBottomTabNavigator();
 export const MaterialBottomTabs = withLayoutContext<
   MaterialBottomTabNavigationOptions,
-  typeof Navigator,
+  typeof Navigator
 >(Navigator);
 
 function TabsLayout() {
   return (
     <>
-    <Stack.Screen
+      <Stack.Screen
         options={{
           headerShown: false,
         }}
       />
-    <MaterialBottomTabs initialRouteName="index" 
-    activeColor="#f0edf6"
-    inactiveColor="#113547"
-    barStyle={{ backgroundColor: '#277ca5' }}
-  >
-      <MaterialBottomTabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color }) => (
-            <AntDesign name="home" size={24} color={color} />
-          ),
-        }}
-      />
-      <MaterialBottomTabs.Screen
-        name="transfer"
-        options={{
-          title: "Transfer",
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons name="compare-arrows" size={24} color={color} />
-          ),
-        }}
-      />
-      <MaterialBottomTabs.Screen
-        name="settings"
-        options={{
-          title: "Settings",
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons name="settings" size={24} color={color} />
-          ),
-        }}
-      />
-      <MaterialBottomTabs.Screen
-        name="balance"
-        options={{
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons name="balance" size={24} color={color} />
-          ),
-        }}
-      />
-      <MaterialBottomTabs.Screen
-        name="notifications"
-        options={{
-          title: "Notifications",
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons name="notifications" size={24} color={color} />
-          ),
-        }}
-      />
-    </MaterialBottomTabs>
+      <MaterialBottomTabs
+        initialRouteName="index"
+        activeColor="#f0edf6"
+        inactiveColor="#113547"
+        barStyle={{ backgroundColor: "#277ca5" }}
+      >
+        <MaterialBottomTabs.Screen
+          name="index"
+          options={{
+            title: "Home",
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="home" size={24} color={color} />
+            ),
+          }}
+        />
+        <MaterialBottomTabs.Screen
+          name="transfer"
+          options={{
+            title: "Transfer",
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="compare-arrows" size={24} color={color} />
+            ),
+          }}
+        />
+        <MaterialBottomTabs.Screen
+          name="settings"
+          options={{
+            title: "Settings",
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="settings" size={24} color={color} />
+            ),
+          }}
+        />
+        <MaterialBottomTabs.Screen
+          name="balance"
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="balance" size={24} color={color} />
+            ),
+          }}
+        />
+        <MaterialBottomTabs.Screen
+          name="notifications"
+          options={{
+            title: "Notifications",
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="notifications" size={24} color={color} />
+            ),
+          }}
+        />
+      </MaterialBottomTabs>
     </>
   );
 }
