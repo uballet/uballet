f6e543cfc0907f3df779bcefe3c561725aab3236
fpondarts <flperez@fi.uba.ar>
2024-11-07 10:30:41 -0300

    Refactored recover with seedphrase / view recovery code screens

--- frontend/src/app/(auth)/account-recovery/seedphrase.tsx
+++ frontend/src/app/(auth)/account-recovery/seedphrase.tsx
@@ -1,8 +1,8 @@
-import { Text, View } from "react-native";
-import { Button, TextInput } from "react-native-paper";
-import { useState } from "react";
+import { KeyboardAvoidingView, SafeAreaView, ScrollView, TextInput as RNTextInput, View } from "react-native";
+import { Button, TextInput, Text } from "react-native-paper";
+import React, { useEffect, useRef, useState } from "react";
 import { useSeedPhraseRecovery } from "../../../hooks/useSeedPhraseRecovery";
-import { Redirect } from "expo-router";
+import { Redirect, useRouter } from "expo-router";
 import { useLogout } from "../../../hooks/useLogout";
 
 
@@ -17,38 +17,95 @@ function ErrorText({ isError }: { isError: boolean }) {
     )
 }
 export default function SeedPhraseRecoveryScreen() {
-    const [seedPhrase, setSeedphrase] = useState('')
     const { recover, isPending, isSuccess, isError } = useSeedPhraseRecovery()
-    const logout = useLogout()
+    React.createRef()
+    const [mnemonic, setMnemonic] = useState(new Array(12).fill(''))
+    const [refs, setRefs] = useState<React.RefObject<RNTextInput>[]>(new Array(12).fill(null))
+    const router = useRouter();
+
+    useEffect(() => {
+        setRefs(refs => refs.map((ref, index) => {
+            return React.createRef()
+        }))
+    }, [])
+
+    function onSetMnemonic(mnemonicWord: string, index: number) {
+        const newMnemonic = [...mnemonic]
+        newMnemonic[index] = mnemonicWord.trim()
+        setMnemonic(newMnemonic)
+    }
     const onButtonPress = () => {
-        recover({ code: seedPhrase })
+        recover({ code: mnemonic.join(' ') })
     }
 
     if (isSuccess) {
         return <Redirect href={'/(auth)/'} />
     }
+
     return (
-        <View className="flex-1 items-center justify-center">
-            <Text className="m-2">Recover with seedphrase</Text>
-            <TextInput
-                multiline={true}
-                numberOfLines={5}
-                returnKeyType="done"
-                blurOnSubmit
-                mode="outlined"
-                className="w-3/4 h-40"
-                value={seedPhrase}
-                placeholder="Enter seed phrase"
-                onChangeText={setSeedphrase}
-                autoCapitalize="none"
-            />
-            <ErrorText isError={isError} />
-            <Button disabled={isPending} onPress={onButtonPress} mode="contained" className="m-2">
-                <Text>Recover</Text>
-            </Button>
-            <Button testID="logout-button" mode="outlined" className="m-4 w-2/3" onPress={logout}>
-                <Text className="text-red-700">Log Out</Text>
-            </Button>
-        </View>
+        <SafeAreaView className="flex-1">
+            <Text className="m-2 self-center" variant="titleLarge">Recover with seedphrase</Text>
+            <View className="flex-row">
+                <View className="w-1/2">
+                    {mnemonic.slice(0, 6).map((word, index) => (
+                        <View key={`word-${index}`} className="flex-row items-center m-2">
+                            <Text>Word: {index + 1}</Text>
+                            <TextInput
+                                testID={`seed-phrase-input-${index}`}
+                                returnKeyType="next"
+                                key={`word-${index}`}
+                                blurOnSubmit
+                                ref={refs[index]}
+                                mode="outlined"
+                                className="flex-1 mx-2"
+                                autoComplete="off"
+                                onSubmitEditing={() => {
+                                    console.log({ ref: refs[index + 1] })
+                                    console.log({ index})
+                                    refs[index + 1]?.current?.focus()
+                                }}
+                                autoCorrect={false}
+                                value={word}
+                                placeholder={"Word " + (index + 1)}
+                                onChangeText={(v) => onSetMnemonic(v, index)}
+                                autoCapitalize="none"
+                            />
+                        </View>
+                    ))}
+                </View>
+                <View className="w-1/2">
+                    {mnemonic.slice(6, 12).map((word, index) => (
+                        <View key={`word-${index + 6}`} className="flex-row items-center m-2">
+                            <Text>Word: {index + 7}</Text>
+                            <TextInput
+                                testID={`seed-phrase-input-${index + 6}`}
+                                key={`word-${index + 6}`}
+                                returnKeyType="done"
+                                blurOnSubmit
+                                ref={refs[index + 6]}
+                                mode="outlined"
+                                onSubmitEditing={() => refs[index + 7]?.current?.focus?.()}
+                                className="flex-1 mx-2"
+                                autoComplete="off"
+                                autoCorrect={false}
+                                value={word}
+                                placeholder={"Word " + (index + 7)}
+                                onChangeText={(v) => onSetMnemonic(v, index + 6)}
+                                autoCapitalize="none"
+                            />
+                        </View>
+                    ))}
+                </View>
+            </View>
+            <View>
+                <ErrorText isError={isError} />
+                <Button disabled={isPending || mnemonic.some(word => !word)} onPress={onButtonPress} mode="contained" className="m-2 w-2/3 self-center">
+                    Recover
+                </Button>
+                <Button testID="logout-button" mode="outlined" className="m-4 w-2/3 self-center" onPress={router.canGoBack() ? router.back : () => router.replace('/(auth)/')}>
+                    <Text className="text-red-700">Back</Text>
+                </Button>
+            </View>
+        </SafeAreaView>
     );
 }
--- frontend/src/app/(auth)/initialize-account/remember.tsx
+++ frontend/src/app/(auth)/initialize-account/remember.tsx
@@ -1,10 +1,10 @@
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import { ActivityIndicator, Modal, TextInput } from "react-native-paper";
 import { Button } from "react-native-paper";
 import { useEffect, useState } from "react";
-import { useAccountContext } from "../../../hooks/useAccountContext";
 import { useRouter } from "expo-router";
 import { useSignerStore } from "../../../hooks/useSignerStore";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RememberScreen() {
     const router = useRouter()
@@ -22,37 +22,47 @@ export default function RememberScreen() {
         loadSeedphrase().then(code => setMnemonic(code!))
     }
 
-    useEffect(() => {
-        if (!modalVisible) {
-            setMnemonic('')
-        }
-    }, [modalVisible])
-
     const onModalDone = () => {
         router.replace('/(auth)/')
     }
+
+    if (!mnemonic) {
+        return (
+            <View className="flex-1 items-center justify-center">
+                <ActivityIndicator />
+            </View>
+        )
+    }
+    
+    const mnemonicWords = mnemonic.split(' ');
     
     return (
-        <View className="flex-1 items-center justify-center">
-            <Text className="m-2">
-                You'll need the following code in order to recover your account in case of changing/lossing your device
-            </Text>
-            <Text className="m-2">
-                COPY AND SAVE SOMEWHERE SAFE!
-            </Text>
-            {!mnemonic && (
-                <ActivityIndicator />
-            )}
-            {mnemonic && (
-                <View testID="mnemonic-text" className="m-2 p-2 border h-64 bg-gray-200 rounded-md">
-                    <Text className="p-2 text-black">
-                        {mnemonic}
-                    </Text>
+        <SafeAreaView className="flex-1 h-full">
+            <ScrollView className="flex-1 h-full" contentContainerStyle={{ padding: 8, justifyContent: 'space-between' }}>
+                <View className="flex-1 justify-between">
+                    <View className="self-center items-center">
+                        <Text className="m-2">
+                            You'll need the following code in order to recover your account in case of changing/lossing your device
+                        </Text>
+                        <Text className="m-2">
+                            COPY AND SAVE SOMEWHERE SAFE!
+                        </Text>
+                    </View>
+                    {mnemonicWords.map((word, index) => (
+                        <View key={index} >
+                            <View className="flex-row items-center">
+                                <Text>Word {index + 1}:</Text>
+                                <Text testID={`mnemonic-word-${index}`} className="flex-1 m-2 p-2 border border-gray-700 bg-gray-200 rounded-md text-black">
+                                    {word}
+                                </Text>
+                            </View>
+                        </View>
+                    ))}
+                    <Button className="mt-4" testID="mnemonic-done-button" mode="contained" onPress={onOpenModal}>
+                        <Text>Done</Text>
+                    </Button>
                 </View>
-            )}
-            <Button testID="mnemonic-done-button" mode="contained" onPress={onOpenModal}>
-                <Text>Done</Text>
-            </Button>
+            </ScrollView>
             <Modal
                 style={{ justifyContent: "center", alignItems: "center" }}
                 visible={modalVisible}
@@ -60,22 +70,23 @@ export default function RememberScreen() {
             >
                 <View className="items-center justify-center p-8 rounded-md bg-white border">
                     <Text>Confirm you're done saving the code</Text>
-                    <Text>Input the word COPIED</Text>
+                    <Text>Input the 3rd word of your recovery phrase</Text>
                     <TextInput
                         testID="mnemonic-done-input"
                         mode="outlined"
+                        autoCapitalize="none"
                         placeholder="Input the word COPIED"
                         className="m-2 w-[90%]"
                         value={modalText}
                         onChangeText={setModalText}
                     />
                     <View className="flex-row">
-                        <Button testID="mnemonic-confirm-done-button" mode="contained" onPress={onModalDone} disabled={modalText !== 'COPIED'}>
+                        <Button testID="mnemonic-confirm-done-button" mode="contained" onPress={onModalDone} disabled={modalText !== mnemonicWords[2]}>
                             <Text>Done</Text>
                         </Button>
                     </View>
                 </View>
             </Modal>
-        </View>
+        </SafeAreaView>
     )
 }
